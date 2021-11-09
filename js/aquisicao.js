b860d719cbba6e193634f305744108b581e10231
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2021-11-09 12:58:18 -0300

    added temp and umid

--- js/aquisicao.js
+++ js/aquisicao.js
@@ -2,7 +2,9 @@ let x=0;
 
 var intervalo = setInterval(()=>
 {   
-    grafico1(x++, ((Math.random()*20)-10));
-    grafico2(x++, ((Math.random()*2)-1));
+    //grafico1(x++, ((Math.random()*20)-10));
+    //grafico2(x++, ((Math.random()*2)-1));
+    grafico1(x++, temperaturaInt);
+    grafico2(x++, umidadeInt);
 }
 , 500);
--- js/serial.js
+++ js/serial.js
@@ -1,16 +1,24 @@
 const serialport = require('serialport')
-
 //let x=0
 const Readline = require('@serialport/parser-readline')
 const port = new serialport('/dev/ttyUSB0', {baudRate:9600})
 const parser = new Readline()
+
+var temperaturaInt = 0;
+var umidadeInt = 0;
+var temperaturaStr = 0;
+var umidadeStr = 0;
+
 port.pipe(parser)
 parser.on('data', (line)=>
 {
 	console.log("Recebe: " + (line));
 	var leituraDHT11 = line.split('|');
-	console.log("Temp:" + leituraDHT11[0]);
-	console.log("Umid:" + leituraDHT11[1]);
-	//grafico(x++, (line))
+	temperaturaInt = leituraDHT11[0];
+	umidadeInt = leituraDHT11[1];
+	temperaturaStr += String(temperaturaInt)+'\n';
+	umidadeStr += String(umidadeInt)+'\n';
+	console.log("Temp:" + temperaturaStr);
+	console.log("Umid:" + umidadeStr);
 })
 
